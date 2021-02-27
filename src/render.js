8ce00a394715d3ec190a2faee350a7fd0e1bacde
Benjamin Sloan <brsloan@gmail.com>
2021-02-27 11:26:02 -0500

    word count done

--- src/render.js
+++ src/render.js
@@ -526,6 +526,28 @@ const Quill = require('quill');
   });
 
 function showWordCount(){
+  removeElementsByClass('popup');
+  var popup = document.createElement("div");
+  popup.classList.add("popup");
+
+  var chapTotalDisplay = document.createElement('p');
+  chapTotalDisplay.innerText = "Chapter Word Count: Calculating...";
+  popup.appendChild(chapTotalDisplay);
+
+  var totalDisplay = document.createElement('p');
+  totalDisplay.innerText = "Total Word Count: Calculating...";
+  popup.appendChild(totalDisplay);
+
+  var closeBtn = document.createElement("input");
+  closeBtn.type = "button";
+  closeBtn.value = "Close";
+  closeBtn.onclick = function(){
+    popup.remove();
+  };
+  popup.appendChild(closeBtn);
+
+  document.body.appendChild(popup);
+
   var activeTotal = countWords(editorQuill.getText());
 
   var total = 0;
@@ -533,8 +555,9 @@ function showWordCount(){
       var text = convertToPlainText(chap.contents ? chap.contents : chap.getFile());
       total += countWords(text);
   });
-  console.log("Chapter Word Count: " + activeTotal);
-  console.log("Total Word count: " + total);
+
+  chapTotalDisplay.innerText = "Chapter Word Count: " + activeTotal;
+  totalDisplay.innerText = "Total Word Count: " + total;
 };
 
 function countWords(s){
@@ -810,6 +833,7 @@ function openAProject(docPath) {
     displayProject();
   }
 }
+
 
 
 //*******/
