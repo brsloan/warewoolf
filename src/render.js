3f70fd20abde365985b3a6bc0e891569384ce51e
Benjamin Sloan <brsloan@gmail.com>
2022-03-17 11:23:44 -0400

    Rewrote tool for breaking headings into chapters

--- src/render.js
+++ src/render.js
@@ -513,36 +513,51 @@ const quillToWord = require('quill-to-word');
     breakHeadingsIntoChapters();
   });
 
+
 function breakHeadingsIntoChapters(headingLevel = 1){
-  var textDelta = editorQuill.getContents();
-  var generatedChaps = [];
-  var newChapIndex = -1;
+  var totalText = editorQuill.getText();
+  var onHeader = false;
+  var headerIndices = [];
 
-  for(i=0; i < textDelta.ops.length; i++){
-    var thisOp = textDelta.ops[i];
-    var nextOp = textDelta.ops[i + 1];
 
-    if(nextOp && nextOp.attributes && nextOp.attributes.header && nextOp.attributes.header == headingLevel){
-      generatedChaps.push([thisOp]);
-      newChapIndex++;
+  for(let i = 0 ; i < totalText.length; i++){
+    var frmt = editorQuill.getFormat(i, 1);
+
+    if(frmt.header && frmt.header == headingLevel){
+      if(onHeader == false){
+        onHeader = true;
+        headerIndices.push(i);
+      }
     }
     else {
-      if(newChapIndex == -1){
-        generatedChaps.push([]);
-        newChapIndex++;
-      }
-      generatedChaps[newChapIndex].push(thisOp);
+      onHeader = false;
     }
   }
 
-  if(generatedChaps.length > 1){
-    editorQuill.setContents({ops: generatedChaps.shift()}, "user");
+  console.log(headerIndices);
 
+
+  if(headerIndices.length > 0){
+    var generatedChaps = [];
+
+    //Add beginning fragment if it does not begin with a heading
+    if(headerIndices[0] != 0)
+      generatedChaps.push(editorQuill.getContents(0, headerIndices[0]));
+    //Add middle chapters
+    for(let i = 0; i < headerIndices.length - 1; i++){
+      var chapLength = headerIndices[i + 1] - headerIndices[i];
+      generatedChaps.push(editorQuill.getContents(headerIndices[i], chapLength));
+    }
+    //Add last chapter from index to end of delta
+    generatedChaps.push(editorQuill.getContents(headerIndices[headerIndices.length - 1]))
+
+    editorQuill.setContents(generatedChaps.shift(), "user");
     generatedChaps.forEach(function(chap){
       const titleCharacterLimit = 100;
-      addImportedChapter({ops: chap}, chap[0].insert.slice(0,titleCharacterLimit));
+      addImportedChapter(chap, chap.ops[0].insert.slice(0,titleCharacterLimit));
     });
   }
+
 }
 
 function convertFirstLinesToTitles(){
