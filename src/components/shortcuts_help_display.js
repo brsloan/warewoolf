33ebe32a391682c3b1cbeed8d166ac7895a260fc
Benjamin Sloan <brsloan@gmail.com>
2022-05-16 10:44:57 -0400

    update shortcuts help

--- src/components/shortcuts_help_display.js
+++ src/components/shortcuts_help_display.js
@@ -13,7 +13,18 @@ function showShortcutsHelp(){
     "<h2>Alteration</h2>" +
     "<p>Move Chapter Up: Ctrl + Shift + Up</p>" +
     "<p>Move Chapter Down: Ctrl + Shift + Down</p>" +
-    "<p>Change Chapter Title: Ctrl + Shift + Left</p>";
+    "<p>Change Chapter Label: Ctrl + Shift + Left</p>" +
+    "<h2>Formatting</h2>" +
+    "<p>Title (Heading 1, Centered): Ctrl + T</p>" +
+    "<p>Headings: Ctrl + (1-4)</p>" +
+    "<p>Left Align: Ctrl + L</p>" +
+    "<p>Right Align: Ctrl + R</p>" +
+    "<p>Center Align: Ctrl + E</p>" +
+    "<p>Justify Align: Ctrl + J</p>" +
+    "<p>Strikethrough: Ctrl + -</p>" +
+    "<p>Italics: Ctrl + I</p>" +
+    "<p>Bold: Ctrl + B</p>" +
+    "<p>Underline: Ctrl + U</p>";
 
     var closeBtn = createButton("Close");
     closeBtn.onclick = function(){
