03755b01d1c222aec1dd861a414daac2b60b04a0
Benjamin Sloan <brsloan@gmail.com>
2022-05-17 09:47:44 -0400

    change compile defaults to docx

--- src/components/compile_display.js
+++ src/components/compile_display.js
@@ -3,14 +3,14 @@ function showCompileOptions(docPath){
     var popup = document.createElement("div");
     popup.classList.add("popup");
     var compileForm = document.createElement("form");
-  
+
     var typeLabel = document.createElement("label");
     typeLabel.innerText = "File Type: ";
     typeLabel.for = "filetype-select";
     compileForm.appendChild(typeLabel);
-  
+
     var typeSelect = document.createElement("select");
-    const typeOptions = [".txt", ".docx"];
+    const typeOptions = [".docx", ".txt"];
     typeOptions.forEach(function(op){
       var txtOp = document.createElement("option");
       txtOp.value = op;
@@ -18,46 +18,46 @@ function showCompileOptions(docPath){
       typeSelect.appendChild(txtOp);
     });
     compileForm.appendChild(typeSelect);
-  
+
     compileForm.appendChild(document.createElement('br'));
-  
+
     var insertStrLabel = document.createElement("label");
     insertStrLabel.innerText = "Insert string to mark chapter breaks: ";
     insertStrLabel.for = "insert-str-input";
     compileForm.appendChild(insertStrLabel);
-  
+
     var insertStrInput = document.createElement("input");
     insertStrInput.type = "text";
-    insertStrInput.value = "##";
+    insertStrInput.value = "";
     insertStrInput.id = "insert-str-input";
     compileForm.appendChild(insertStrInput);
-  
+
     compileForm.appendChild(document.createElement('br'));
-  
+
     var insertHeadLabel = document.createElement("label");
     insertHeadLabel.innerText = "Insert chapter titles as headings: ";
     insertHeadLabel.for = "insert-head-check";
     compileForm.appendChild(insertHeadLabel);
-  
+
     var insertHeadCheck = document.createElement("input");
     insertHeadCheck.type = "checkbox";
     //insertHeadCheck.value = "insert-head";
     insertHeadCheck.id = "insert-head-check";
     compileForm.appendChild(insertHeadCheck);
-  
+
     compileForm.appendChild(document.createElement('br'));
-  
+
     var compileBtn = document.createElement("input");
     compileBtn.type = "submit";
     compileBtn.value = "Compile";
     compileForm.appendChild(compileBtn);
-  
+
     var cancelBtn = createButton("Cancel");
     cancelBtn.onclick = function(){
       popup.remove();
     };
     compileForm.appendChild(cancelBtn);
-  
+
     compileForm.onsubmit = function(){
       var options = {
         type: typeSelect.value,
@@ -67,10 +67,10 @@ function showCompileOptions(docPath){
       getCompileFilepath(options, docPath);
       popup.remove();
     };
-  
+
     popup.appendChild(compileForm);
     document.body.appendChild(popup);
-  
+
   }
 
   function getCompileFilepath(options, docPath){
