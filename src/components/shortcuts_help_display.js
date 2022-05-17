47a2d883af8511c43bb34da272946a58eee06015
Benjamin Sloan <brsloan@gmail.com>
2022-05-17 17:00:01 -0400

    updated shortcuts display

--- src/components/shortcuts_help_display.js
+++ src/components/shortcuts_help_display.js
@@ -6,26 +6,32 @@ function showShortcutsHelp(){
     popup.innerHTML =
     "<h1>Shortcuts</h1>" +
     "<h2>Navigation</h2>" +
-    "<p>View Previous Chapter: Ctrl + Up</p>" +
-    "<p>View Next Chapter: Ctrl + Down</p>" +
-    "<p>Shift Focus To Editor: Ctrl + Left</p>" +
-    "<p>Shift Focus To Notes: Ctrl + Right</p>" +
+    "<table class='shortcuts-table'>" +
+    "<tr><td>View Previous Chapter</td><td>Ctrl + Up</td></tr>" +
+    "<tr><td>View Next Chapter</td><td>Ctrl + Down</td></tr>" +
+    "<tr><td>Shift Focus To Editor</td><td>Ctrl + Left</td></tr>" +
+    "<tr><td>Shift Focus To Notes</td><td>Ctrl + Right</td></tr>" +
+    "</table>" +
     "<h2>Alteration</h2>" +
-    "<p>Move Chapter Up: Ctrl + Shift + Up</p>" +
-    "<p>Move Chapter Down: Ctrl + Shift + Down</p>" +
-    "<p>Change Chapter Label: Ctrl + Shift + Left</p>" +
+    "<table class='shortcuts-table'>" +
+    "<tr><td>Move Chapter Up</td><td>Ctrl + Shift + Up</td></tr>" +
+    "<tr><td>Move Chapter Down</td><td>Ctrl + Shift + Down</td></tr>" +
+    "<tr><td>Change Chapter Label</td><td>Ctrl + Shift + Left</td></tr>" +
+    "</table>" +
     "<h2>Formatting</h2>" +
-    "<p>Title (Heading 1, Centered): Ctrl + T</p>" +
-    "<p>Headings: Ctrl + (1-4)</p>" +
-    "<p>Clear Heading: Ctrl + 0</p>" +
-    "<p>Left Align: Ctrl + L</p>" +
-    "<p>Right Align: Ctrl + R</p>" +
-    "<p>Center Align: Ctrl + E</p>" +
-    "<p>Justify Align: Ctrl + J</p>" +
-    "<p>Strikethrough: Ctrl + -</p>" +
-    "<p>Italics: Ctrl + I</p>" +
-    "<p>Bold: Ctrl + B</p>" +
-    "<p>Underline: Ctrl + U</p>";
+    "<table class='shortcuts-table'>" +
+    "<tr><td>Title (Heading 1, Centered)</td><td>Ctrl + T</td></tr>" +
+    "<tr><td>Headings</td><td>Ctrl + (1-4)</td></tr>" +
+    "<tr><td>Clear Heading</td><td>Ctrl + 0</td></tr>" +
+    "<tr><td>Left Align</td><td>Ctrl + L</td></tr>" +
+    "<tr><td>Right Align</td><td>Ctrl + R</td></tr>" +
+    "<tr><td>Center Align</td><td>Ctrl + E</td></tr>" +
+    "<tr><td>Justify Align</td><td>Ctrl + J</td></tr>" +
+    "<tr><td>Strikethrough</td><td>Ctrl + -</td></tr>" +
+    "<tr><td>Italics</td><td>Ctrl + I</td></tr>" +
+    "<tr><td>Bold</td><td>Ctrl + B</td></tr>" +
+    "<tr><td>Underline</td><td>Ctrl + U</td></tr>" +
+    "</table>";
 
     var closeBtn = createButton("Close");
     closeBtn.onclick = function(){
