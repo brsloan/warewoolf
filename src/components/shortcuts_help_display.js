3390d46bbca4100a07b14af764b1cfa7d1dbba51
Benjamin Sloan <brsloan@gmail.com>
2022-05-19 21:05:42 -0400

    refactoring shortcuts help display

--- src/components/shortcuts_help_display.js
+++ src/components/shortcuts_help_display.js
@@ -3,35 +3,66 @@ function showShortcutsHelp(){
     var popup = document.createElement("div");
     popup.classList.add("popup", "popup-shortcuts");
 
-    popup.innerHTML =
-    "<h1>Shortcuts</h1>" +
-    "<h2>Navigation</h2>" +
-    "<table class='shortcuts-table'>" +
-    "<tr><td>View Previous Chapter</td><td>Ctrl + Up</td></tr>" +
-    "<tr><td>View Next Chapter</td><td>Ctrl + Down</td></tr>" +
-    "<tr><td>Shift Focus To Editor</td><td>Ctrl + Left</td></tr>" +
-    "<tr><td>Shift Focus To Notes</td><td>Ctrl + Right</td></tr>" +
-    "</table>" +
-    "<h2>Alteration</h2>" +
-    "<table class='shortcuts-table'>" +
-    "<tr><td>Move Chapter Up</td><td>Ctrl + Shift + Up</td></tr>" +
-    "<tr><td>Move Chapter Down</td><td>Ctrl + Shift + Down</td></tr>" +
-    "<tr><td>Change Chapter Label</td><td>Ctrl + Shift + Left</td></tr>" +
-    "</table>" +
-    "<h2>Formatting</h2>" +
-    "<table class='shortcuts-table'>" +
-    "<tr><td>Title (Heading 1, Centered)</td><td>Ctrl + T</td></tr>" +
-    "<tr><td>Headings</td><td>Ctrl + (1-4)</td></tr>" +
-    "<tr><td>Clear Heading</td><td>Ctrl + 0</td></tr>" +
-    "<tr><td>Left Align</td><td>Ctrl + L</td></tr>" +
-    "<tr><td>Right Align</td><td>Ctrl + R</td></tr>" +
-    "<tr><td>Center Align</td><td>Ctrl + E</td></tr>" +
-    "<tr><td>Justify Align</td><td>Ctrl + J</td></tr>" +
-    "<tr><td>Strikethrough</td><td>Ctrl + -</td></tr>" +
-    "<tr><td>Italics</td><td>Ctrl + I</td></tr>" +
-    "<tr><td>Bold</td><td>Ctrl + B</td></tr>" +
-    "<tr><td>Underline</td><td>Ctrl + U</td></tr>" +
-    "</table>";
+    var shortcuts = [
+      {
+        title: "Navigation",
+        shortcuts: [
+          ['View Previous Chapter', 'Ctrl + Up'],
+          ['View Next Chapter', 'Ctrl + Down'],
+          ['Shift Focus To Editor', 'Ctrl + Left'],
+          ['Shift Focus To Notes', 'Ctrl + Right']
+        ]
+      },
+      {
+        title: "Alteration",
+        shortcuts: [
+          ['Move Chapter Up', 'Ctrl + Shift + Up'],
+          ['Move Chapter Down', 'Ctrl + Shift + Down'],
+          ['Change Chapter Label', 'Ctrl + Shift + Left']
+        ]
+      },
+      {
+        title: "Formatting",
+        shortcuts: [
+          ['Title (Heading 1, Centered)', 'Ctrl + T'],
+          ['Headings', 'Ctrl + (1-4)'],
+          ['Clear Heading', 'Ctrl + 0'],
+          ['Left Align', 'Ctrl + L'],
+          ['Right Align', 'Ctrl + R'],
+          ['Center Align', 'Ctrl + E'],
+          ['Justify Align', 'Ctrl + J'],
+          ['Strikethrough', 'Ctrl + -'],
+          ['Italics', 'Ctrl + I'],
+          ['Bold', 'Ctrl + B'],
+          ['Underline', 'Ctrl + U']
+        ]
+      }
+    ];
+
+    shortcuts.forEach(function(short){
+      var title = document.createElement('h2');
+      title.innerText = short.title;
+      popup.appendChild(title);
+
+      var shortcutsTable = document.createElement('table');
+      shortcutsTable.classList.add('shortcuts-table');
+
+      short.shortcuts.forEach(function(cut){
+        var row = document.createElement('tr');
+
+        var shortLabel = document.createElement('td');
+        shortLabel.innerText = cut[0];
+        row.appendChild(shortLabel);
+
+        var shortKeys = document.createElement('td');
+        shortKeys.innerText = cut[1];
+        row.appendChild(shortKeys);
+
+        shortcutsTable.appendChild(row);
+      });
+
+      popup.appendChild(shortcutsTable);
+    });
 
     var closeBtn = createButton("Close");
     closeBtn.onclick = function(){
