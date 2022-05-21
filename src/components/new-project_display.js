8534ff465e4ba12ed979e85cb62299cab899ec5d
Benjamin Sloan <brsloan@gmail.com>
2022-05-21 19:13:52 -0400

    refactoring

---
+++ src/components/new-project_display.js
@@ -0,0 +1,35 @@
+function requestProjectTitle(callback){
+  var popup = document.createElement("div");
+  popup.classList.add("popup");
+
+  var titleForm = document.createElement("form");
+
+  var message = document.createElement("label");
+  message.innerText = "What is the title of this project?";
+  message.for = "title-input";
+  titleForm.appendChild(message);
+
+  var titleInput = document.createElement("input");
+  titleInput.type = "text";
+  titleInput.placeholder = "Mrs. Dalloway 2: Back In Action";
+  titleInput.id = "title-input";
+  titleForm.appendChild(titleInput);
+
+  var createButton = document.createElement("input");
+  createButton.type = "submit";
+  createButton.value = "Create"
+  titleForm.onsubmit = function(){
+    var title;
+    if(titleInput.value != "")
+      title = titleInput.value;
+    else
+      title = "New Project";
+    popup.remove();
+    callback(title);
+  }
+
+  titleForm.appendChild(createButton);
+  popup.appendChild(titleForm);
+  document.body.appendChild(popup);
+  titleInput.focus();
+}
