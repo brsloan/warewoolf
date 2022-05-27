6f4e1d4f2c96fe4e3a3c0755e9fbb7ab54833c97
Benjamin Sloan <brsloan@gmail.com>
2022-05-27 09:23:41 -0400

    inline styling works

--- src/components/markdownFic.js
+++ src/components/markdownFic.js
@@ -16,8 +16,9 @@ function parseMDF(str){
       tempQuill.insertText(tempQuill.getLength() - 1, parsedLine.line, parsedLine.format.name, parsedLine.format.value);
   });
 
-  return tempQuill.getContents();
 
+
+  return formatAllInline(tempQuill.getContents());
 }
 
 function parseLine(line){
@@ -96,4 +97,70 @@ function parseLine(line){
   }
 
   return parsedLine;
+}
+
+function formatAllInline(delt){
+  var styleMarkers = [
+    {
+      marker: '**',
+      style: 'bold'
+    },
+    {
+      marker: '*',
+      style: 'italic'
+    },
+    {
+      marker: '~~',
+      style: 'strike'
+    }
+  ];
+
+  var packagedDelta = {
+    changed: 0,
+    delta: delt
+  };
+
+  styleMarkers.forEach((marker, i) => {
+    packagedDelta = formatMarkedSegments(packagedDelta.delta, marker.marker, marker.style);
+  });
+
+
+  return packagedDelta.delta;
+}
+
+function formatMarkedSegments(delt, marker, style){
+  marker = marker.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+  var markerRegx = new RegExp(marker + '([^' + marker + ']+)' + marker);
+  console.log(markerRegx);
+
+  var tempQuill = getTempQuill();
+  tempQuill.setContents(delt);
+  var text = tempQuill.getText();
+
+  var foundIndex = 0;
+  var startingIndex = 0;
+  var matchResult;
+  var markedText = "";
+  var counter = 0;
+
+  while(foundIndex > -1){
+
+      matchResult = text.match(markerRegx);
+      foundIndex = matchResult ? matchResult.index : -1;
+
+      if(foundIndex > -1){
+          counter++;
+
+          tempQuill.deleteText(foundIndex, matchResult[0].length);
+          tempQuill.insertText(foundIndex, matchResult[1]);
+          tempQuill.formatText(matchResult.index, matchResult[1].length, style, true);
+          startingIndex = foundIndex + matchResult[1].length;
+          text = tempQuill.getText();
+      }
+  }
+
+  return {
+    changed: counter,
+    delta: tempQuill.getContents()
+  }
 }
