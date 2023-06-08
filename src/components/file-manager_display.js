23714f86537bdf54659af76f7eb2a646c0dc178f
Benjamin Sloan <brsloan@gmail.com>
2023-06-08 11:59:14 -0400

    basic file browsing works

--- src/components/file-manager_display.js
+++ src/components/file-manager_display.js
@@ -1,12 +1,49 @@
 function showFileManager(docPath){
+    docPath = convertFilepath(docPath);
     
     removeElementsByClass('popup');
     var popup = document.createElement("div");
     popup.classList.add("popup");
 
-    var fileList = document.createElement("ul");
-    popup.appendChild(fileList);
+    var currentDirDisplay = document.createElement("p");
+    currentDirDisplay.innerText = docPath;
+    popup.appendChild(currentDirDisplay);
 
+    var selectFieldsContainer = document.createElement("div");
+    selectFieldsContainer.classList.add("file-select-container");
+    popup.appendChild(selectFieldsContainer);
+
+    var dirShortcutList = document.createElement("select");
+    dirShortcutList.classList.add("file-dir-shortcuts");
+    dirShortcutList.size = 20;
+    selectFieldsContainer.appendChild(dirShortcutList);
+
+    var fileListSelect = document.createElement("select");
+    fileListSelect.multiple = true;
+    fileListSelect.classList.add("file-manager-list");
+    selectFieldsContainer.appendChild(fileListSelect);
+    fileListSelect.addEventListener('keydown', function(e){
+        if(e.key === "Enter"){
+            var selectedFiletype = fileListSelect.options[fileListSelect.selectedIndex].dataset.filetype;
+            if (selectedFiletype == "dir"){
+                populateFileList(currentDirDisplay.innerText + "/" + fileListSelect.value, fileListSelect, currentDirDisplay);
+            }
+            else if(fileListSelect.value == "uplevel"){
+                populateFileList(getParentDirectory(currentDirDisplay.innerText), fileListSelect, currentDirDisplay);
+            }
+        }
+    });
+
+    popup.appendChild(document.createElement('br'));
+
+    var newFolder = createButton("New Folder");
+    popup.appendChild(newFolder);
+
+    var rename = createButton("Rename");
+    popup.appendChild(rename);
+
+    var deleteBtn = createButton("Delete");
+    popup.appendChild(deleteBtn);
 
     var close = createButton("Close");
     close.onclick = function(){
@@ -14,21 +51,63 @@ function showFileManager(docPath){
     };
     popup.appendChild(close);
 
-    populateFileList(docPath, fileList);
+    populateShortcutsList(dirShortcutList);
+    populateFileList(docPath, fileListSelect, currentDirDisplay);
 
     document.body.appendChild(popup);
-    close.focus();
+    fileListSelect.focus();
 }
 
-function populateFileList(docPath, listElement){
-    var files = getFileList(docPath);
+function getParentDirectory(filepath){
+    var cutIndex = filepath.lastIndexOf('/');
+    return cutIndex > -1 ? filepath.slice(0,cutIndex) : filepath;
+}
 
-    files.forEach(file => {
-        var fileLabel = document.createElement("li");
-        fileLabel.innerText = file.name;
-        listElement.appendChild(fileLabel);
-        console.log(file);
+function populateShortcutsList(listElement){
+    var shortcuts = getShortcutsFake();
+    for(i=0;i<shortcuts.length;i++){
+        var shortcut = document.createElement("option");
+        shortcut.value = shortcuts[i];
+        shortcut.innerText = "> " + shortcuts[i].split("/").pop();
+        listElement.appendChild(shortcut);
+    }
+}
+
+function getShortcutsFake(){
+    return [
+        "/usr/name",
+        "/usr/name/documents",
+        "usr/media/thumbdrive1",
+        "usr/media/thumbdrive2"
+    ]
+}
+
+function populateFileList(directoryPath, listElement, currentDirDisplay){
+    currentDirDisplay.innerText = directoryPath;
+    
+    listElement.innerHTML = "";
+
+    var files = getFileList(directoryPath);
+    files.sort(function(a,b){
+        return b.isDirectory() - a.isDirectory();
     });
+
+    var parentDir = document.createElement("option");
+    parentDir.value = "uplevel";
+    parentDir.innerText = "< Parent Directory";
+    listElement.appendChild(parentDir);
+
+
+    for(i=0;i<files.length;i++){
+        var filename = document.createElement("option");
+        filename.value = files[i].name;
+        filename.innerText = (files[i].isDirectory() ? "> " : "") + files[i].name;
+        filename.dataset.filetype = files[i].isDirectory() ? "dir" : "file";
+
+        listElement.appendChild(filename);
+    }
+
+    parentDir.selected = true;
 }
 
 function getFileList(dirPath){
@@ -37,8 +116,4 @@ function getFileList(dirPath){
     } catch (err) {
         logErrror(err);
     }
-}
-
-function packageFileList(files){
-
 }
