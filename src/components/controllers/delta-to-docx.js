874dc85307f239a3c4c35ec93d91e31940e7119d
brsloan <brsloan@gmail.com>
2024-02-25 17:56:38 -0500

    can export footnotes to docx

--- src/components/controllers/delta-to-docx.js
+++ src/components/controllers/delta-to-docx.js
@@ -19,14 +19,77 @@ function packageDocxBase64(doc, callback){
 function convertDeltaToDocx(delt){
   var parsedQuill = quillParser.parseQuillDelta(delt);
 
-  var xParagraphs = [];
+  var fnoteParRegx = /^\[\^.+]:/gm;
 
+  var footnoteBodies = [];
+  var nonfootnoteParas = [];
+
+  //first, extract footnote body paragraphs
   parsedQuill.paragraphs.forEach(function(para){
+    if(para.textRuns && para.textRuns.length > 0 && para.textRuns[0].text)
+    {
+      var thisMarker = fnoteParRegx.exec(para.textRuns[0].text);
+      if(thisMarker){
+        console.log('we have a footnote! marker: ' + thisMarker);
+        var xRuns = [];
+        para.textRuns[0].text = para.textRuns[0].text.replace(thisMarker, '');
+
+        para.textRuns.forEach(function(run){
+          var xRunAttributes = convertRunAtttributes(run.attributes);
+          xRunAttributes.text = run.text;
+          xRuns.push(new docx.TextRun(xRunAttributes));
+        });
+
+        var xParaAttributes = convertParaAttributes(para.attributes);
+        xParaAttributes.children = xRuns;
+
+        footnoteBodies.push({
+          marker: thisMarker[0].slice(0,-1),
+          paras: [ new docx.Paragraph(xParaAttributes) ]
+        });
+      }
+      else {
+        nonfootnoteParas.push(para);
+      }
+    }
+    else {
+      nonfootnoteParas.push(para);
+    }
+  });
+
+  var xParagraphs = [];
+  var fnoteMarkerRegx = /\[\^.+]/gm;
+
+  nonfootnoteParas.forEach(function(para){
     var xRuns = [];
     para.textRuns.forEach(function(run){
-      var xRunAttributes = convertRunAtttributes(run.attributes);
-      xRunAttributes.text = run.text;
-      xRuns.push(new docx.TextRun(xRunAttributes));
+      var fnoteMarker = fnoteMarkerRegx.exec(run.text);
+
+      //If run has a footnote marker, split into 2 runs with marker between.
+      if(fnoteMarker){
+        var cutPoint = run.text.indexOf(fnoteMarker[0]);
+        var text1 = run.text.slice(0, cutPoint);
+        var text2 = run.text.slice(cutPoint + fnoteMarker[0].length)
+        var xRun1Attr = convertRunAtttributes(run.attributes);
+        xRun1Attr.text = text1;
+        var xRun2Attr = convertRunAtttributes(run.attributes);
+        xRun2Attr.text = text2;
+
+        var fnoteBodyNum = footnoteBodies.findIndex(function(fn, i, arr){
+          return fn.marker == fnoteMarker[0];
+        }) + 1;
+
+        var fnMarkerRun = new docx.FootnoteReferenceRun(fnoteBodyNum);
+
+        xRuns.push(new docx.TextRun(xRun1Attr));
+        xRuns.push(fnMarkerRun);
+        xRuns.push(new docx.TextRun(xRun2Attr));
+      }
+      else {
+        var xRunAttributes = convertRunAtttributes(run.attributes);
+        xRunAttributes.text = run.text;
+        xRuns.push(new docx.TextRun(xRunAttributes));
+      }
     });
 
     var xParaAttributes = convertParaAttributes(para.attributes);
@@ -34,6 +97,14 @@ function convertDeltaToDocx(delt){
 
     xParagraphs.push(new docx.Paragraph(xParaAttributes));
   });
+
+  var footnotes = {};
+
+  for(i=0;i<footnoteBodies.length;i++){
+    footnotes[i + 1] = {
+      children: footnoteBodies[i].paras
+    }
+  }
 
   const doc = new docx.Document({
     creator: project.author,
@@ -70,6 +141,7 @@ function convertDeltaToDocx(delt){
         }
       ]
     },
+    footnotes: footnotes,
     sections: [
       {
         properties: {
