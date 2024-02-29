a1a6aa302866e2f993c6794eac7e3ad4e04f101d
brsloan <brsloan@gmail.com>
2024-02-29 10:23:52 -0500

    cleanup

--- src/components/controllers/docx-import.js
+++ src/components/controllers/docx-import.js
@@ -258,12 +258,9 @@ function parseDocx(xmlStr){
   const parser = new DOMParser();
   const doc = parser.parseFromString(xmlStr, "application/xml");
 
-  // print the name of the root element or error message
   const errorNode = doc.querySelector("parsererror");
   if (errorNode) {
     console.log("error while parsing");
-  } else {
-    console.log(doc.documentElement.nodeName);
   }
 
   return doc;
