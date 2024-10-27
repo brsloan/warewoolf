72005d43c1013df85a34665d539dc680bc8bf1a6
brsloan <brsloan@gmail.com>
2024-10-27 16:08:58 -0400

    corkboard parsing works

---
+++ src/components/controllers/corkboard.js
@@ -0,0 +1,57 @@
+const newCard = require('../models/corkboard-card');
+const fs = require('fs');
+const { logError } = require('../controllers/error-log');
+const cardsFilename = 'project_corkboard.txt';
+
+function getCardsFromFile(chaptersPath){
+    console.log('get cards from file...');
+    const cardsFilepath = chaptersPath + cardsFilename;
+    try {
+        var cardsString = fs.readFileSync(cardsFilepath, "utf8");
+        return parseCardsString(cardsString);
+    }
+    catch(err){
+        logError(err);
+    }
+}
+
+function parseCardsString(str){
+    //Cards saved as one markdown file with labels as headings and descriptions as pargraphs. Colors can be indicated immediately after heading marker
+    //in this way: "# [1] Label Text". Number corresponds to preset color, 0=default, which doesn't have to be included.
+
+    let firstLabel = /^# (.+)\n\n/;
+    let label = /^# (.+)\n\n/gm;
+    let blankLines = /(?:\r?\n){2,}/gm;
+    let colorNum = /^\[(\d)\] /; 
+
+
+    //escape JSON chars
+    str = str.replaceAll('\\','\\\\');
+    str = str.replaceAll('/','\\/');
+    str = str.replaceAll('"','\\"');
+    str = str.replaceAll('\t','\\t'); 
+
+    str = str.replace(firstLabel, '[{"label":"$1", "descr":"');
+    str = str.replace(label, '"}, {"label":"$1", "descr":"');
+    str = str.replace(blankLines, '\\n');
+    str = str + '"}]';
+
+    var rawCards = JSON.parse(str);
+
+    for(i=0;i<rawCards.length;i++){
+        var color = rawCards[i].label.match(colorNum);
+        if(color){
+            rawCards[i].label = rawCards[i].label.replace(colorNum, '');
+            rawCards[i].color = color[1];
+        }
+        else {
+            rawCards[i].color = 0;
+        }
+    }
+
+    console.log(rawCards);
+
+    return JSON.parse(str);
+}
+
+module.exports = getCardsFromFile;
---
+++ src/components/models/corkboard-card.js
@@ -0,0 +1,11 @@
+
+
+function newCard(label, description, color = 0){
+    return {
+        label: label,
+        descr: description,
+        color: color
+    }
+}
+
+module.exports = newCard;
