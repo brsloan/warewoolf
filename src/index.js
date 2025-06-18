134fcad1db46f33da6a7d97276f6200427d14e35
brsloan <brsloan@gmail.com>
2025-06-18 10:44:58 -0400

    fixed error window creation timing

--- src/index.js
+++ src/index.js
@@ -362,10 +362,23 @@ const createWindow = () => {
   Menu.setApplicationMenu(menu);
 };
 
+const assignActiveAndCreateWindow = () => {
+  //Have to assign this after ready to avoid error when opened too quickly via dock on macOS
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+
+  createWindow();
+};
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', assignActiveAndCreateWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -373,14 +386,6 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
   }
 });
 
