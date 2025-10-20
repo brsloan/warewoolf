594fa163f2971394f1a89393c6e1a565473b5ac7
brsloan <brsloan@gmail.com>
2025-10-20 12:59:21 -0400

    rm markers for forced scene headers and transitions

--- src/components/controllers/screenplay.js
+++ src/components/controllers/screenplay.js
@@ -3,11 +3,12 @@ function parseFountain(str){
     str = convertAllLineBreaks(str);
     //console.log(str);
 
-    let sceneHeader = /(?<=\n|^)(([iI][nN][tT]|[eE][xX][tT]|[^\w][eE][sS][tT]|\.|[iI]\.?\/[eE]\.?)([^\n]+))\n/g; //Not multi-line because ^ is checking very first line in document
+    let sceneHeader = /(?<=\n|^)(([iI][nN][tT]|[eE][xX][tT]|[^\w][eE][sS][tT]|[iI]\.?\/[eE]\.?)([^\n]+))\n/g; //Not multi-line because ^ is checking very first line in document
+    let forcedSceneHeader = /(?<=\n|^)\.([^\n]+)\n/gm;
     let character = /(?<=\n)([ \t]*[^<>a-z\s\/\n][^<>a-z:!\?\n]*[^<>a-z\(!\?:,\n\.][ \t]?)\n{1}(?!\n)/g;
     let parenthetical = /^(\s*\([^<>\n]*?\)[\s]?)\n/gm;
     let transition = /\n([\*_]*([^<>\na-z]*TO:|FADE TO BLACK\.|FADE OUT\.|CUT TO BLACK\.)[\*_]*)\n/g;
-    let forcedTransition = /\n((&gt;|>)\s*[^<>\n]+)\n/g;
+    let forcedTransition = /\n>(\s*[^<>\n]+)\n/g;
 
     let dialog = /(?<="character-cue":true}},\n|"parenthetical-block":true}},\n)([^{]*?)\n(?=\n|{)/g; //Must be run after character & parenthetical since it matches on their results
     let falseTransition = /\n(>\s*[^<>\n]+(<\s*))\n/g; //For centered actions that may appear to be transitions??? Taken from Fountain github
@@ -20,6 +21,7 @@ function parseFountain(str){
     str = str.replaceAll('"','\\"');
 
     str = str.replace(sceneHeader, '\n{"insert":"$1"},{"insert":"\\n","attributes":{"scene-header":true}},');
+    str = str.replace(forcedSceneHeader, '\n{"insert":"$1"},{"insert":"\\n","attributes":{"scene-header":true}},');
     str = str.replace(character, '{"insert":"$1"},{"insert":"\\n","attributes":{"character-cue":true}},\n');
     str = str.replace(parenthetical, '{"insert":"$1","attributes":{"parenthetical-block":true}},{"insert":"\\n","attributes":{"parenthetical-block":true}},\n');
     str = str.replace(transition, '\n{"insert":"$1","attributes":{"transition-block":true}},{"insert":"\\n","attributes":{"transition-block":true}},');
