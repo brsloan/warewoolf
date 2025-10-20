13871620033c451c00685bd7b12bca72f37b8cf9
brsloan <brsloan@gmail.com>
2025-10-20 14:51:35 -0400

    inline formats WIP

--- src/components/controllers/screenplay.js
+++ src/components/controllers/screenplay.js
@@ -1,3 +1,4 @@
+const { getTempQuill } = require('./quill-utils');
 
 function parseFountain(str){
     str = convertAllLineBreaks(str);
@@ -14,7 +15,6 @@ function parseFountain(str){
     //let falseTransition = /\n(>\s*[^<>\n]+(<\s*))\n/g; //For centered actions that may appear to be transitions??? Taken from Fountain github
     let action = /(?<=}},\n)([^{}]*)(?=\n{2}|\n{)/g;
     let anythingLeft = /^\s*([^{}\n]+)$/gm;
-
 
       //escape JSON chars (except Tab, which will do later so can be detected with Regexes)
     str = str.replaceAll('\\','\\\\');
@@ -48,6 +48,24 @@ function parseFountain(str){
     let cleanup = /{"insert":"","attributes":{"action-block":true}},{"insert":"\\n","attributes":{"action-block":true}},/g;
     str = str.replace(cleanup,'');
 
+    let boldItalicUnderline = /(_\*{3}|\*{3}_)([^{}]+)(_\*{3}|\*{3}_)/g;
+    let boldItalic = /(\*{3})([^{}]+)(\*{3})/g;
+    let boldUnderline = /(_\*{2}|\*{2}_)([^{}]+)(_\*{2}|\*{2}_)/g;
+    let italicUnderline = /(_\*{1}|\*{1}_)([^{}]+)(_\*{1}|\*{1}_)/g;
+    let bold = /(\*{2})([^{}]+)(\*{2})/g;
+    let italic = /(?<!\\)(\*{1})([^{}]+?)(\*{1})/g;
+    let underline = /(_)([^{}_]+)(_)/g;
+
+    console.log('before styling:\n' + str);
+
+    str = str.replace(boldItalicUnderline, '"},{"insert":"$2","attributes":{"bold":"true","italic":"true","underline":"true"}},{"insert":"');
+    str = str.replace(boldItalic, '"},{"insert":"$2","attributes":{"bold":"true","italic":"true"}},{"insert":"');
+    str = str.replace(boldUnderline, '"},{"insert":"$2","attributes":{"bold":"true","underline":"true"}},{"insert":"');
+    str = str.replace(italicUnderline, '"},{"insert":"$2","attributes":{"italic":"true","underline":"true"}},{"insert":"');
+    str = str.replace(underline, '"},{"insert":"$2","attributes":{"underline":"true"}},{"insert":"');
+    str = str.replace(bold, '"},{"insert":"$2","attributes":{"bold":"true"}},{"insert":"');
+    str = str.replace(italic, '"},{"insert":"$2","attributes":{"italic":"true"}},{"insert":"');
+    
     console.log('after condensing newlins etc:\n' + str);
     return JSON.parse(str);
 }
@@ -55,6 +73,46 @@ function parseFountain(str){
 function convertAllLineBreaks(text) {
     return text.replace(/\r\n|\r/g, '\n');
 }
+
+function styleInlineMarkers(delt){
+    let boldItalicUnderline = /(_\*{3}|\*{3}_)([^{}]+)(_\*{3}|\*{3}_)/g;
+    let boldItalic = /(\*{3})([^{}]+)(\*{3})/g;
+    let boldUnderline = /(_\*{2}|\*{2}_)([^{}]+)(_\*{2}|\*{2}_)/g;
+    let italicUnderline = /(_\*{1}|\*{1}_)([^{}]+)(_\*{1}|\*{1}_)/g;
+    let bold = /(\*{2})([^{}]+)(\*{2})/g;
+    let italic = /(?<!\\)(\*{1})([^{}]+?)(\*{1})/g;
+    let underline = /(_)([^{}_]+)(_)/g;
+
+    var tempQuill = getTempQuill();
+
+};
+
+function styleMarkedSpans(tempQuill, delt, markerRegx, style){  
+   //var tempQuill = getTempQuill();
+    tempQuill.setContents(delt);
+    var text = tempQuill.getText();
+  
+    var foundIndex = 0;
+    var startingIndex = 0;
+    var matchResult;
+    var markedText = "";
+  
+    while(foundIndex > -1){
+  
+        matchResult = text.match(markerRegx);
+        foundIndex = matchResult ? matchResult.index : -1;
+  
+        if(foundIndex > -1){
+            tempQuill.deleteText(foundIndex, matchResult[0].length);
+            tempQuill.insertText(foundIndex, matchResult[2]);
+            tempQuill.formatText(matchResult.index, matchResult[2].length, style, true);
+            startingIndex = foundIndex + matchResult[1].length;
+            text = tempQuill.getText();
+        }
+    }
+  
+    return tempQuill.getContents();
+  }
 
 module.exports = {
     parseFountain
